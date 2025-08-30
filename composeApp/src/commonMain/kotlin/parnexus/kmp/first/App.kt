bec9725f207bc39686a46c834fad69e3a393f833
Parinexus <Parinexus@gmail.com>
2025-08-30 18:17:37 +0330

    Add light and darl colors and theme

--- composeApp/src/commonMain/kotlin/parnexus/kmp/first/App.kt
+++ composeApp/src/commonMain/kotlin/parnexus/kmp/first/App.kt
@@ -2,9 +2,12 @@ package parnexus.kmp.first
 
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import parnexus.kmp.first.theme.CoinTheme
 
 @Composable
 @Preview
 fun App() {
+    CoinTheme {
 
+    }
 }
