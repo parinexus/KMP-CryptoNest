fb426def4b7528cc48c2d1b3c2ade68f4bfb15dd
Parinexus <Parinexus@gmail.com>
2025-08-31 18:11:34 +0330

    Create HttpClientFactory to configure Ktor client

---
+++ composeApp/src/commonMain/kotlin/parinexus/kmp/first/core/network/HttpClientFactory.kt
@@ -0,0 +1,37 @@
+package parinexus.kmp.first.core.network
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.cache.HttpCache
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.http.headers
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import parinexus.kmp.first.AppSecrets
+
+object HttpClientFactory {
+    fun create(engine: HttpClientEngine): HttpClient {
+        return HttpClient(engine) {
+            install(ContentNegotiation) {
+                json(
+                    json = Json {
+                        ignoreUnknownKeys = true
+                    }
+                )
+            }
+            install(HttpTimeout) {
+                socketTimeoutMillis = 20_000L
+                requestTimeoutMillis = 20_000L
+            }
+            install(HttpCache)
+            defaultRequest {
+                headers { append("x-access-token", AppSecrets.apiKey) }
+                contentType(ContentType.Application.Json)
+            }
+        }
+    }
+}
