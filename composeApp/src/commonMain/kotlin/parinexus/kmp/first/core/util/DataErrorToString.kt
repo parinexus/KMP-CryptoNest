8bd1b3e5a296f8c0819de43441806110883f4688
Parinexus <Parinexus@gmail.com>
2025-09-23 10:51:19 +0330

    Create data error to string resource mapper and number formatters

---
+++ composeApp/src/commonMain/kotlin/parinexus/kmp/first/core/util/DataErrorToString.kt
@@ -0,0 +1,27 @@
+package parinexus.kmp.first.core.util
+
+import kmp_cryptonest.composeapp.generated.resources.Res
+import kmp_cryptonest.composeapp.generated.resources.error_disk_full
+import kmp_cryptonest.composeapp.generated.resources.error_insufficient_balance
+import kmp_cryptonest.composeapp.generated.resources.error_no_internet
+import kmp_cryptonest.composeapp.generated.resources.error_request_timeout
+import kmp_cryptonest.composeapp.generated.resources.error_serialization
+import kmp_cryptonest.composeapp.generated.resources.error_too_many_requests
+import kmp_cryptonest.composeapp.generated.resources.error_unknown
+import parinexus.kmp.first.core.domain.DataError
+import org.jetbrains.compose.resources.StringResource
+
+fun DataError.toUiText(): StringResource {
+    val stringRes = when(this) {
+        DataError.Local.DISK_FULL -> Res.string.error_disk_full
+        DataError.Local.UNKNOWN -> Res.string.error_unknown
+        DataError.Remote.REQUEST_TIMEOUT -> Res.string.error_request_timeout
+        DataError.Remote.TOO_MANY_REQUESTS -> Res.string.error_too_many_requests
+        DataError.Remote.NO_INTERNET -> Res.string.error_no_internet
+        DataError.Remote.SERVER -> Res.string.error_unknown
+        DataError.Remote.SERIALIZATION -> Res.string.error_serialization
+        DataError.Remote.UNKNOWN -> Res.string.error_unknown
+        DataError.Local.INSUFFICIENT_FUNDS -> Res.string.error_insufficient_balance
+    }
+    return stringRes
+}
