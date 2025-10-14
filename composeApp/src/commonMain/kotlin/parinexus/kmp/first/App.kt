d6245a059b01bd4acc70528362f10b9d80960d3b
Parinexus <Parinexus@gmail.com>
2025-10-14 15:06:56 +0330

    feat: Implement PortfolioScreen in App composable

--- composeApp/src/commonMain/kotlin/parinexus/kmp/first/App.kt
+++ composeApp/src/commonMain/kotlin/parinexus/kmp/first/App.kt
@@ -3,12 +3,17 @@ package parinexus.kmp.first
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import parinexus.kmp.first.coins.presentation.CoinsGridScreen
+import parinexus.kmp.first.portfolio.presentation.PortfolioScreen
 import parinexus.kmp.first.theme.CoinTheme
 
 @Composable
 @Preview
 fun App() {
     CoinTheme {
-        CoinsGridScreen({})
+//        CoinsGridScreen({})
+        PortfolioScreen(
+            onCoinItemClicked = {},
+            onDiscoverCoinsClicked = {}
+        )
     }
 }
